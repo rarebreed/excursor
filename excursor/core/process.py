5dc6b39b9fc0242786675f746f0a912055c2b3cd
Sean Toner <placeoftheway@gmail.com>
2023-08-17 08:30:02 -0400

    Made installer more robust.  More documentation in notebook

--- excursor/core/process.py
+++ excursor/core/process.py
@@ -3,65 +3,12 @@ the child process output and continue with other tasks.
 """
 
 import asyncio
-from asyncio import Future, SubprocessTransport
 from asyncio.subprocess import Process
 from dataclasses import dataclass, field
 import os
 from pathlib import Path
 from subprocess import PIPE
-from typing import IO, Any, Literal, Self
-
-
-@dataclass
-class ChildProcess(asyncio.SubprocessProtocol):
-    exit_future: Future
-    save = False
-    encoding = "utf8"
-    data = bytearray()
-    show_out = True
-
-    def pipe_data_received(self, fd: int, data: bytes):
-        if self.save:
-            self.data += data
-        if self.show_out:
-            print(f"{fd}: " + data.decode(self.encoding), end=None)
-
-    def process_exited(self):
-        self.exit_future.set_result(True)
-
-
-@dataclass
-class ProcessResult:
-    future: Future
-    proto: ChildProcess
-    transport: SubprocessTransport
-
-    def is_done(self):
-        return self.future.done()
-
-    def exit_code(self):
-        return self.transport.get_returncode()
-
-    def output(self) -> str:
-        return self.proto.data.decode(self.encoding)
-
-    def is_ok(self, val=0, throw=False):
-        ret = self.exit_code()
-
-        successful = False
-        match [ret, throw]:
-            case [None, _]:
-                print("Process has not finished yet")
-            case [code, True] if code != val:
-                raise Exception(
-                    f"Process failed with exit code {code} which was not success of {val}")
-            case [code, False] if code != val:
-                print(
-                    f"Process failed with exit code {code} which was not success of {val}")
-            case _:
-                print(f"Process was successful with exit code {code}")
-                successful = True
-        return successful
+from typing import IO, Any, Literal
 
 
 @dataclass
@@ -89,80 +36,6 @@ class Run:
 
         if self.env is None:
             self.env = os.environ
-
-    def w_cmd(self, prog: str) -> Self:
-        self.cmd = prog
-        return self
-
-    def w_args(self, *args: str) -> Self:
-        self.args = args
-        return self
-
-    def w_stdin(self, pipe: int | IO[Any]) -> Self:
-        self.stdin = pipe
-        return self
-
-    def w_stdout(self, pipe: int | IO[Any]) -> Self:
-        self.stdout = pipe
-        return self
-
-    def w_stderr(self, pipe: int | IO[Any]) -> Self:
-        self.stderr = pipe
-        return self
-
-    def w_cwd(self, dir: str | Path) -> Self:
-        self.dir = dir
-        return self
-
-    def w_shell(self, sh: bool) -> Self:
-        self.shell = sh
-        return self
-
-    def w_busize(self, size: int) -> Self:
-        self.bufsize = size
-        return self
-
-    def w_text(self, txt: bool) -> Self:
-        self.text = txt
-        return self
-
-    def w_env(self, env: dict[str]) -> Self:
-        self.env = env
-        return self
-
-    def build(self):
-        if not self.cmd:
-            raise Exception("Must supply a cmd string")
-        return self
-
-    async def launch(self):
-        self.build()
-        loop = asyncio.get_running_loop()
-        exit_future = Future(loop=loop)
-
-        kwargs = self.__dict__.copy()
-        cmd: str = kwargs.pop("cmd")
-        kwargs.pop("sudo")
-        args: list[str] = kwargs.pop("args")
-
-        runner = loop.subprocess_exec
-        if self.shell:
-            cmd = f"{cmd} {' '.join(args)}"
-            args = []
-            runner = loop.subprocess_shell
-
-        print(f"Executing: {cmd} {kwargs}")
-
-        transport, protocol = await runner(
-            lambda: ChildProcess(exit_future),
-            cmd,
-            *args,
-            **kwargs
-        )
-
-        await exit_future
-        transport.close()
-        return ProcessResult(future=exit_future, proto=protocol, transport=transport)
 
     def __call__(self, *, pw: str | None = None):
         if self.shell:
@@ -252,13 +125,11 @@ class Run:
 if __name__ == "__main__":
     async def run1():
         runner1 = Run("iostat", ["2", "2"])
-        proc = await runner1.launch()
-        proc.is_ok()
+        await runner1.run()
 
     async def run2():
         runner2 = Run("echo 'hi sean' > hi.text", shell=True)
-        proc2 = await runner2.launch()
-        proc2.is_ok()
+        await runner2.run()
 
     async def main():
         run = Run(cmd="ls", args=["-al", "/usr/local"], shell=True)
