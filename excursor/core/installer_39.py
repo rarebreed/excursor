e33b66f6eaaa62252f483682860d08cf865119c5
Sean Toner <placeoftheway@gmail.com>
2023-08-19 10:08:56 -0400

    week1 added

--- excursor/core/installer_39.py
+++ excursor/core/installer_39.py
@@ -22,7 +22,7 @@ from pathlib import Path
 import platform
 from typing import Literal
 
-from excursor.core.process import Run
+from excursor.core.process_39 import Run
 
 
 @dataclass(kw_only=True)
