b99e4800f7900be2a1e055b5fdc82457e5854a54
Sean Toner <placeoftheway@gmail.com>
2025-03-01 19:59:10 -0500

    more statistics notes. functor and monad

--- excursor/func.py
+++ excursor/func.py
@@ -2,7 +2,7 @@
 from abc import abstractmethod
 import asyncio
 from dataclasses import dataclass
-from typing import Callable, Coroutine, Iterable, Protocol
+from typing import Callable, Coroutine, Iterable, Protocol, Self
 
 
 class Functor[T](Protocol):
@@ -12,8 +12,23 @@ class Functor[T](Protocol):
         ...
 
 
+class Applicative[T](Functor[T], Protocol):
+
+    @classmethod
+    def lift(cls, a: T) -> Self:
+        ...
+
+
+class Monad[T](Functor[T], Protocol):
+
+    # this took a bit of playing with to figure out
+    # @abstractmethod
+    def flat_map[R, M: "Monad"](self, fun: Callable[[T], M]) -> M:
+        ...
+
+
 @dataclass
-class Maybe[T](Functor[T]):
+class Maybe[T](Monad[T]):
     inner: T | None
 
     def map[R](self, fun: Callable[[T], R | None]) -> "Maybe[R]":
@@ -68,10 +83,96 @@ class Iter[T](Functor[T]):
     def map[R](self, fun: Callable[[T], R]) -> "Iter[R]":
         return Iter(inner=(fun(x) for x in self.inner))
 
-    def take(self, num: int):
-        if num < 1:
-            raise Exception("num arg must be >= 1")
-        return Iter(inner=(i for ind, i in enumerate(self.inner) if ind < num))
+    def take(self, until: int | Callable[[T], bool]) -> "Iter[T]":
+        """Includes items from until
+
+        If until is a callable, it will include elements up until it gets a False result.  As soon as it gets a single
+        False, no other elements will be in the returned Iter.  It is the opposite of drop
+
+        Parameters
+        ----------
+        until : int | Callable[[T], bool]
+            _description_
+
+        Returns
+        -------
+        Iter[T]
+            _description_
+
+        Raises
+        ------
+        Exception
+            if until is less than 1
+        """
+        match until:
+            case int():
+                if until < 1:
+                    raise Exception("num arg must be >= 1")
+                return Iter(inner=(i for ind, i in enumerate(self.inner) if ind < until))
+            case fn:
+                def gen():
+                    for i in self.inner:
+                        if not fn(i):
+                            yield i
+                        else:
+                            break
+                return Iter(inner=gen())
+
+    def drop(self, until: int | Callable[[T], bool]) -> "Iter[T]":
+        """Excludes elements while until returns True.
+
+        If until is a callable, the first time it gets a False result, it will always include any other items in inner.
+        It is the opposite of take.
+
+        Parameters
+        ----------
+        until : int | Callable[[T], bool]
+            if an int, up to that many elements excluded. If a callable, stops dropping when evals to False
+
+        Returns
+        -------
+        Iter[T]
+            _description_
+
+        Raises
+        ------
+        Exception
+            if until is negative
+        """
+        match until:
+            case int():
+                if until < 0:
+                    raise Exception("can not drop negative amount of items")
+                return Iter(inner=(x for i, x in enumerate(self.inner) if i >= until))
+            case fn:
+                def gen():
+                    matched = True
+                    for i in self.inner:
+                        if matched:
+                            matched = fn(i)
+                        if not matched:
+                            yield i
+                return Iter(inner=gen())
+
+    def collect[R](self, ret: Callable[[Iterable], R]) -> R:
+        return ret(self.inner)
+
+    def where(self, select: Callable[[T], bool]) -> Self:
+        """Returns a new Iter, where each element returned True when passed to the select predicate
+
+        Called a `filter` in other APIs, but filter sounds like you do NOT include the elements,
+
+        Parameters
+        ----------
+        select : Callable[[T], bool]
+            predicate function. If it retuens True, keep the item, else exclude it
+
+        Returns
+        -------
+        Self
+        """
+        self.inner = (i for i in self.inner if select(i))
+        return self
 
 
 print(f"{__name__}")
