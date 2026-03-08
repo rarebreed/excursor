66dea574906881baae694612be2b2d295f0f6d00
Sean Toner <placeoftheway@gmail.com>
2026-03-07 22:45:11 -0500

    feat: Introduce LanceDB for local data storage and update project dependencies.

--- excursor/database.py
+++ excursor/database.py
@@ -1,5 +1,12 @@
 """
 Database module for excursor.
 
-Stores data in a local
+Stores data in a local lancedb
 """
+
+from typing import Final
+
+import lance
+import lancedb
+
+uri: Final[str] = "game_catalog"
